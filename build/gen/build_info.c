2b724562e05903ba067a8e76a031d9dbf3f9746b
Grigoriy Betsan <griggors@gmail.com>
2018-12-31 00:39:19 +0200

    C version

---
+++ build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20181230-223023";
+const char *build_timestamp = "2018-12-30T22:30:23Z";
+const char *build_version = "1.0";
---
+++ build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20181230-223022/2.10.2-master-g31f9fe698";
+const char *mg_build_timestamp = "2018-12-30T22:30:22Z";
+const char *mg_build_version = "2.10.2";
