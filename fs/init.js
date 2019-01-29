2b125837509bb3fdddd1fbe2ffd2187fccdafa83
Grigoriy Betsan <griggors@gmail.com>
2019-01-29 23:24:41 +0200

    Basic func.

--- fs/init.js
+++ fs/init.js
@@ -1,11 +1,36 @@
 load('api_config.js');
 load('api_rpc.js');
 load('api_timer.js');
+load('api_arduino_ssd1306.js');
 
 let getTemp = ffi('float getTemp(void)');
 let getHumd = ffi('float getHumd(void)');
+let getECO2 = ffi('float getECO2(void)');
+let getTVOC = ffi('float getTVOC(void)');
+//let timer   = ffi('void timer_cb(void)');
+
+let disp = Adafruit_SSD1306.create_i2c(-1, Adafruit_SSD1306.RES_128_64);
+disp.begin(Adafruit_SSD1306.SWITCHCAPVCC, 0x3C, true)
+disp.clearDisplay()
+disp.setCursor(0, 0);
+disp.setTextColor(Adafruit_SSD1306.WHITE);
+disp.setTextSize(3);
+disp.setTextWrap(true);
+disp.write("TRHC");
+disp.display();
+
+disp.setTextSize(2);
 
 
-Timer.set(5000, true, function() {
-  print('Hello,', getTemp());
+Timer.set(1000, true, function() {
+  disp.clearDisplay();
+  disp.setCursor(0, 0);
+  disp.write(JSON.stringify(getTVOC()));
+  disp.setCursor(64, 0);
+  disp.write(JSON.stringify(getECO2()));
+  disp.setCursor(0, 20);
+  disp.write(JSON.stringify(Math.round(getTemp())));
+  disp.setCursor(64, 20);
+  disp.write(JSON.stringify(Math.round(getHumd())));
+  disp.display();
 }, null);
--- src/main.c
+++ src/main.c
@@ -2,10 +2,21 @@
 #include "mgos_i2c.h"
 #include "mgos_si7021.h"
 #include "mgos_mqtt.h"
-#include "ssd1306.h"
+#include "mgos_ccs811.h"
 
+static struct mgos_ccs811 *s_ccs811;
 static struct mgos_si7021 *s_si7021;
-static struct mgos_ssd1306 *d_ssd1306;
+static struct mgos_i2c *i2c;
+int ret;
+
+
+float getECO2(void) {
+  return mgos_ccs811_get_eco2(s_ccs811);
+}
+
+float getTVOC(void) {
+  return mgos_ccs811_get_tvoc(s_ccs811);
+}
 
 float getTemp(void) {
   return mgos_si7021_getTemperature(s_si7021);
@@ -15,62 +26,54 @@ float getHumd(void) {
   return mgos_si7021_getHumidity(s_si7021);
 }
 
-static void timer_cb(void) {
-  float temperature, humidity;
-  char *d = malloc(34);
+static void timer_cb(void *user_data) {
+  //float temperature, humidity;
+  //char *d = malloc(34);
+  i2c = mgos_i2c_get_global();
 
-  temperature = getTemp();
-  humidity    = getHumd();
+  //mgos_usleep(5000);
+  //uint8_t data[5] = { 0xFF, 0x11, 0xE5, 0x72, 0x8A };
+  //ret = mgos_i2c_write(i2c, 0x5B, data, 5, true);
+  //LOG(LL_INFO, ("CCS811 RESET CODE! :: 0x%02x", ret));
 
-  LOG(LL_INFO, ("si7021 temperature=%.2f humidity=%.2f", temperature, humidity));
-  mgos_ssd1306_clear(d_ssd1306);
-  sprintf(d, "Temp: %.2f C", temperature);
-  mgos_ssd1306_draw_string(d_ssd1306, 0, 0, d);
-  sprintf(d, "Humd: %.2f (RH)", humidity);
-  mgos_ssd1306_draw_string(d_ssd1306, 0, 20, d);
-  mgos_ssd1306_refresh(d_ssd1306, true);
-  free(d);
+  mgos_usleep(5000);
+
+  //ret = mgos_i2c_read_reg_b(i2c, 0x5B, 0x20);
+  //LOG(LL_INFO, ("CCS811 INIT CODE! :: 0x%02x", ret));
+
+  //ret = mgos_i2c_write(i2c, 0x5B, data, 5, true);
+  //LOG(LL_INFO, ("CCS811 RESET CODE! :: 0x%02x", ret));
+
+  float temp = getTemp();
+  float humd    = getHumd();
+  float eco2 = getECO2();
+  float tvoc = getTVOC();
+
+  LOG(LL_INFO, ("ccs811 eco2=%.2f ; tvoc=%.2f", eco2, tvoc));
+  LOG(LL_INFO, ("si7021 temp=%.2f ; humd=%.2f", temp, humd));
+  //sprintf(d, "Temp: %.2f C", temperature);
+  //sprintf(d, "Humd: %.2f (RH)", humidity);
+  //free(d);
+  (void) user_data;
 }
 
 static void tempInit(void) {
-  struct mgos_i2c *i2c;
   i2c = mgos_i2c_get_global();
   if (i2c) {
+    mgos_usleep(5000);
     s_si7021 = mgos_si7021_create(i2c, 0x40); // Default I2C address
+    mgos_set_timer(100000, true, timer_cb, NULL);    
+    mgos_usleep(5000);
+    s_ccs811 = mgos_ccs811_create(i2c, 0x5b); // Default I2C address
+    mgos_ccs811_setDriveMode(s_ccs811, CCS811_DRIVE_MODE_10SEC);
     if (s_si7021) {
-      //mgos_set_timer(1000, true, timer_cb, NULL);
     } else {
       LOG(LL_ERROR, ("Could not initialize sensor"));
     }
   }
 }
 
-void displayInit(void) {
-  if(mgos_ssd1306_init()) {
-    d_ssd1306 = mgos_ssd1306_get_global();
-  } else {
-    LOG(LL_ERROR, ("Could not initiate display"));
-  }
-}
-
-void drawWelcome(void) {
-  char *d = malloc(34);
-  mgos_ssd1306_select_font(d_ssd1306, 1);
-
-  strcpy(d, "MCU ESP8266");
-  mgos_ssd1306_draw_string(d_ssd1306, 32, 32, d);
-
-  strcpy(d, "TRH sensor");
-  mgos_ssd1306_draw_string(d_ssd1306, 32, 44, d);
-  
-  mgos_ssd1306_refresh(d_ssd1306, true);
-  free(d);
-}
-
 enum mgos_app_init_result mgos_app_init(void) {
-  displayInit();
-  drawWelcome();
   tempInit();
-  timer_cb();
   return MGOS_APP_INIT_SUCCESS;
 }
